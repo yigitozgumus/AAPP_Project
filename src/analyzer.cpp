32bb2872228c489f009b3544cf3aaba7ca919f68
Yiğit Özgümüş <yigitozgumus1@gmail.com>
2018-05-25 22:42:39 +0200

    initial commit

---
+++ src/analyzer.cpp
@@ -0,0 +1,8 @@
+/**
+ * This class will test the algorithms with different settings to create a basis for analysis
+ * and a comparison baseline
+ * 
+ */
+class Analyzer{
+
+};
---
+++ src/experiment.cpp
@@ -0,0 +1,6 @@
+/** 
+ *  This class will contain the 3 main algorithm for the computation of the Strongly Connected Component
+ */
+class Scc{
+
+};
