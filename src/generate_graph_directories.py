cadb12b79883261fd6e3b55057849d287acf1d61
Yigit Ozgumus <yigitozgumus1@gmail.com>
2018-06-25 16:23:29 +0200

    fancy new graph generator with arguments and progress bar

--- src/generate_graph_directories.py
+++ src/generate_graph_directories.py
@@ -1,8 +1,11 @@
+#!/usr/bin/python
 import networkx as nx
 import pathlib
 import os
 from contextlib import contextmanager
 import numpy as np
+import argparse
+from time import sleep
 
 
 @contextmanager
@@ -14,44 +17,70 @@ def working_directory(directory):
     finally:
         os.chdir(owd)
 
+# Print iterations progress
 
-def generate_folders():
+
+def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 *
+                                                     (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end='\r')
+    # Print New Line on Complete
+    if (iteration == total):
+        print()
+
+def generate_folders(foldername):
     
-    node_capacity_list = ["5_50","50_100","100_500", "500_1000"]
-    #node_capacity_list = ["5_50"]
+    n_c_list = ["5_50","50_100","100_500", "500_1000"]
+    #n_c_list = ["5_50"]
     number_edge_percentage = 5
-    ncp_v2 = np.array([[x] * number_edge_percentage for x in node_capacity_list])
+    ncp_v2 = np.array([[x] * number_edge_percentage for x in n_c_list])
     ncp_v2 = ncp_v2.flatten()
-    print(ncp_v2)
-    edge_percentage_list = ["0_10","10_25","25_50","50_75","75_100"]
-    ecp_v2 = np.array([edge_percentage_list * len(node_capacity_list)]).flatten()
-    print(ecp_v2)
-    current_directory = os.getcwd() + "/input_v2"
+    print("\nGraph Creation has begun\n")
+    print("The Graph Node Ranges are:")
+    for i in range(len(n_c_list)):
+        nodes = n_c_list[i].split("_")
+        print(i+1, "-) {} - {}".format(int(nodes[0]),int(nodes[1])))
+    e_p_list = ["0_10","10_25","25_50","50_75","75_100"]
+    ecp_v2 = np.array([e_p_list * len(n_c_list)]).flatten()
+    print("\nEdge Creation Percentages are:")
+    for i in range(len(e_p_list)):
+        minmax = e_p_list[i].split("_")
+        print(i+1, "-) {} - {}".format(int(minmax[0]),int(minmax[1])))
+    current_directory = os.getcwd() + "/" + foldername
     pathlib.Path(current_directory).mkdir( exist_ok=True)
-    print(current_directory)
+    print("\nThe graphs will be written into :\n {}".format(current_directory))
     final_directory_list = []
     #generate the first level of folders 
-    print("Seperate directories for the node classes will be created")
-    print("Sub directories with different percentage of edge creations will be created")
-    for ddir in node_capacity_list:
+   
+    for ddir in n_c_list:
         final_directory = os.path.join(current_directory, ddir)
-        #final_directory = current_directory
-        #print(final_directory)
         pathlib.Path(final_directory).mkdir( exist_ok=True)
         #generate the second level of folders
-        final_directory_list.extend([final_directory] * len(edge_percentage_list))
-        # for cdir in edge_percentage_list:
-        #     percentage_folder = os.path.join(final_directory, cdir)
-        #     final_directory_list.append(percentage_folder)
-        #     pathlib.Path(percentage_folder).mkdir(exist_ok=True)
-    #print(final_directory_list)
+        final_directory_list.extend([final_directory] * len(e_p_list))
     results = zip(final_directory_list,ncp_v2,ecp_v2)
-    return results
+    print("\nSeperate directories for the node classes is created\n")
+    total_graph = len(n_c_list) * len(e_p_list)
+    return (results,total_graph)
 
 def generate_graphs(data_pack,graph_num):
-    
-    for ddir,n_r,e_p in data_pack:
-        counter = 0
+    total_graph = data_pack[1] * graph_num
+    printProgressBar(0, total_graph, prefix='Progress:', suffix='Complete', length=100)
+    counter = 0
+    for ddir,n_r,e_p in data_pack[0]:
+        
         with working_directory(ddir):
             #get the node number
             nodes = n_r.split("_")
@@ -69,6 +98,9 @@ def generate_graphs(data_pack,graph_num):
                 name = n_r +"|" + e_p + "|" + str(counter) + ".graph"
                 counter+=1
                 write_graph(graph,node_list[x],name)
+                sleep(0.1)
+                printProgressBar(counter, total_graph, prefix='Progress:', suffix='Complete', length=100)
+    print("\nThe Graps are created with success\n")
                 
 
 def write_graph(graph,node,name):
@@ -79,8 +111,15 @@ def write_graph(graph,node,name):
     f.close()
     
 def main():
-    dir_results = generate_folders()
-    generate_graphs(dir_results,10)
+    parser = argparse.ArgumentParser(description='Generate directed graphs \
+                                        with different edge creation probabilities.')
+    parser.add_argument("graph_count", metavar ='G', help="Number of graphs for each edge creation interval")
+    parser.add_argument("folder", metavar='F', help="The Target Location of the Graph files")
+    args = parser.parse_args()
+    graph_count = args.graph_count
+    relative_folder = args.folder
+    dir_results = generate_folders(relative_folder)
+    generate_graphs(dir_results,int(graph_count))
 
 
 
